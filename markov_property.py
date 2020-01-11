db8db5c31728624cc290947cefc44d7ac227a412
Jang-SY <59248899+Jang-SY@users.noreply.github.com>
2020-01-12 02:49:41 +0900

    Update markov_property.py

--- markov_property.py
+++ markov_property.py
@@ -9,13 +9,13 @@ init_vector1 = np.asarray([0, 1, 0], dtype = float)
 init_vector2 = np.asarray([1, 0, 0], dtype = float)
 init_vector3 = np.asarray([0, 0, 1], dtype = float)
 
-num = 10
+num = 2
 
 for i in range(num):
     init_vector1 = np.dot(init_vector1, P)
     init_vector2 = np.dot(init_vector2, P)
     init_vector3 = np.dot(init_vector3, P)
 
-print('vector1(rain) = ',init_vector1)
-print('vector2(sunny) = ',init_vector2)
-print('vector3(cloudy) = ',init_vector3)
+print('rain ->',num,'days later = ',init_vector1)
+print('sunny ->',num,'days later = ',init_vector2)
+print('cloudy ->',num,'days later = ',init_vector3)
